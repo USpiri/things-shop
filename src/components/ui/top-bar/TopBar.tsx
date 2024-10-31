4fc41e4763c1751601ac1441dee614027d122001
USpiri <urielspiridione135@gmail.com>
2024-10-31 15:12:05 -0300

    ✨ Feat: Sticky topbar

--- src/components/ui/top-bar/TopBar.tsx
+++ src/components/ui/top-bar/TopBar.tsx
@@ -4,7 +4,7 @@ import { MenuTopBar } from "./MenuTopBar";
 
 export const TopBar = () => {
   return (
-    <div className="font-mono h-14 border-b border-neutral-800 flex items-center">
+    <div className="font-mono h-14 border-b border-neutral-800 flex items-center sticky top-0 bg-background z-10">
       <div className="w-full max-w-6xl mx-auto px-5 flex items-center font-extralight justify-between tracking-widest">
         <TitleTopBar />
         <CategoryTopBar />
