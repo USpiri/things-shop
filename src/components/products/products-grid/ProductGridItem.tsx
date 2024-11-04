e90f9956e517e9bb07d50751f1700b8f412aad9f
USpiri <urielspiridione135@gmail.com>
2024-11-04 13:08:48 -0300

    ✨ Feat: Small image size

--- src/components/products/products-grid/ProductGridItem.tsx
+++ src/components/products/products-grid/ProductGridItem.tsx
@@ -15,15 +15,15 @@ export const ProductGridItem = ({ product }: Props) => {
       <div className="relative">
         <Image
           src={`/images/products/${product.images[0]}`}
-          width={500}
-          height={500}
+          width={300}
+          height={300}
           alt={`Product: ${product.title} image`}
           className="w-full"
         />
         <Image
           src={`/images/products/${product.images[1]}`}
-          width={500}
-          height={500}
+          width={300}
+          height={300}
           alt={`Product: ${product.title} image`}
           className="w-full absolute top-0 left-0 transition-opacity opacity-0 group-hover:opacity-100 duration-300"
         />
