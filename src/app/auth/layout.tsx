e1d3898de8301b9539a7b31328477d1c88f88653
USpiri <urielspiridione135@gmail.com>
2024-11-05 11:16:35 -0300

    ✨ Feat: Auth redirect

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,3 +1,12 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await auth();
+  if (session?.user) redirect("/");
   return <>{children}</>;
 }
