06aa3339c89c9ff0260cec4a60bdc0ae2cda734d
USpiri <urielspiridione135@gmail.com>
2024-11-06 20:36:48 -0300

    🐛 Fix: Button text

--- src/app/(shop)/checkout/address/page.tsx
+++ src/app/(shop)/checkout/address/page.tsx
@@ -25,7 +25,7 @@ export default async function Page() {
             form="address-form"
             type="submit"
           >
-            Confirm payment
+            Confirm address
           </Button>
         </section>
       </div>
