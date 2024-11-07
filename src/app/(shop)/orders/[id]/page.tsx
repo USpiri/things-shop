9028e561c645e99dd14982f83a223e8b40db1299
USpiri <urielspiridione135@gmail.com>
2024-11-07 02:38:34 -0300

    ✨ Feat: Order id page

--- src/app/(shop)/orders/[id]/page.tsx
+++ src/app/(shop)/orders/[id]/page.tsx
@@ -1,92 +1,78 @@
-import { SubTitle, Title } from "@/components";
-import { initialData } from "@/seed";
-import Image from "next/image";
-import Link from "next/link";
+import { getOrderById } from "@/actions/order/get-by-id";
+import {
+  ProductSmallItem,
+  Shipping,
+  SubTitle,
+  Summary,
+  Title,
+} from "@/components";
+import { redirect } from "next/navigation";
 
-export default function Page() {
+interface Props {
+  params: {
+    id: string;
+  };
+}
+
+export default async function Page({ params }: Props) {
+  const { id } = params;
+  const res = await getOrderById(id);
+
+  if (!res.ok) redirect("/orders/");
+  const {
+    isPaid,
+    total,
+    subTotal,
+    tax,
+    shipping,
+    OrderAddress: address,
+    OrderItem: products,
+  } = res.order!;
+
   return (
     <main>
       <Title>
-        <span>Order: #123456</span>
+        <span>Order: {id.split("-")[0]}</span>
       </Title>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10 max-w-4xl mx-auto">
-        <aside className="md:col-span-2 border border-rose-600/30 rounded text-sm bg-red-400/10 px-3 py-1.5">
-          Payment pending
-        </aside>
-        <section className="flex flex-col gap-5">
-          <div>
-            <SubTitle className="text-lg font-mono mb-3">Products</SubTitle>
-            <div className="flex flex-col gap-3 mb-6">
-              {initialData.products.slice(5, 13).map((product) => (
-                <article key={product.slug} className="flex">
-                  <Link href={`/product/${product.slug}`} className="shrink-0">
-                    <Image
-                      src={`/images/products/${product.images[0]}`}
-                      width={50}
-                      height={50}
-                      alt={`Product: ${product.title}`}
-                      className="rounded object-cover aspect-square"
-                    />
-                  </Link>
-                  <div className="flex flex-col justify-between flex-1 px-3 pb-0 overflow-hidden">
-                    <header>
-                      <Link href={`/product/${product.slug}`}>
-                        <h3 className="font-mono text-lg tracking-wider truncate">
-                          {product.title}
-                        </h3>
-                      </Link>
-                      <p className="font-mono tracking-widest text-sm opacity-50">
-                        SM
-                        <span> ($50)</span>
-                        <span> x2</span>
-                      </p>
-                    </header>
-                  </div>
-                  <footer className="pt-1 flex flex-col justify-between px-3">
-                    <span>$100</span>
-                  </footer>
-                </article>
-              ))}
-            </div>
-          </div>
+        {!isPaid && (
+          <aside className="md:col-span-2 border border-rose-600/30 rounded text-sm bg-red-400/10 px-3 py-1.5">
+            Payment pending
+          </aside>
+        )}
+        <section className="flex flex-col gap-6">
+          <SubTitle className="text-lg font-mono">Products</SubTitle>
+          {products.map((item) => (
+            <ProductSmallItem
+              key={`${item.size}-${item.product.slug}`}
+              product={{
+                slug: item.product.slug,
+                quantity: item.quantity,
+                price: item.product.price,
+                size: item.size,
+                title: item.product.title,
+                image: item.product.images[0].url,
+                id: "",
+              }}
+            />
+          ))}
         </section>
         <section className="flex flex-col gap-3">
-          <SubTitle className="text-lg font-mono sm:col-span-2">
-            Order summary
-          </SubTitle>
-          <dl className="divide-y divide-neutral-800 text-sm *:flex *:justify-between *:py-4 font-mono">
-            <div className="opacity-70">
-              <dt>Subtotal</dt>
-              <dd>$500</dd>
-            </div>
-            <div className="opacity-70">
-              <dt>Tax</dt>
-              <dd>$0</dd>
-            </div>
-            <div className="opacity-70">
-              <dt>Shipping</dt>
-              <dd>$10</dd>
-            </div>
-            <div className="font-bold text-lg">
-              <dt>Total</dt>
-              <dd>$510</dd>
-            </div>
-          </dl>
-          <SubTitle className="text-lg font-mono">
-            Shipping Information
-          </SubTitle>
-          <div className="text-sm text-neutral-400 font-mono grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 relative">
-            <p>Uriel Spiridione</p>
-            <p>email@example.com</p>
-            <p>+54 1234567890</p>
-            <p>Some street 123</p>
-            <p>Alternative: Some other 456</p>
-            <p>Argentina, Córdoba, Córdoba, X5000</p>
-          </div>
-          <p className="text-xs font-mono mt-6 text-neutral-400">
-            By submitting this page you agree to our{" "}
-            <strong>terms and conditions</strong> and our{" "}
-            <strong>privacy policy</strong>.
+          <Summary
+            total={total}
+            subtotal={subTotal}
+            tax={tax}
+            shipping={shipping}
+          />
+          <Shipping
+            {...address!}
+            country={address!.country.name}
+            address2={address!.address2 ?? undefined}
+            editButton={false}
+          />
+          <p className="text-sm font-mono mt-3 text-neutral-400">
+            <strong>Order ID: </strong>
+            {id}
           </p>
         </section>
       </div>
