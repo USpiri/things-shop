58bc827da21cdaf4c2408898526f5c7c652205e9
USpiri <urielspiridione135@gmail.com>
2024-11-07 01:09:07 -0300

    ✨ Feat: Redirect out of checkout on empty cart

--- src/app/(shop)/checkout/(checkout)/ui/ProductsList.tsx
+++ src/app/(shop)/checkout/(checkout)/ui/ProductsList.tsx
@@ -2,10 +2,14 @@
 import { useEffect, useState } from "react";
 import { ProductItem } from "./ProductItem";
 import { useCartStore } from "@/store";
+import { useRouter } from "next/navigation";
 
 export const ProductsList = () => {
   const products = useCartStore((state) => state.cart);
   const [loaded, setLoaded] = useState(false);
+  const router = useRouter();
+
+  if (useCartStore.getState().cart.length === 0) router.replace("/");
 
   useEffect(() => {
     setLoaded(true);
